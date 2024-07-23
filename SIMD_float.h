77cadfd6f0dfc49d06e9693607048f10a52daa8e
Morty-Mcfly <frostd4713@students.clarkstate.edu>
2024-07-23 18:54:12 -0400

    Enabled SSE2 intrinsics (you're welcome)

--- SIMD_float.h
+++ SIMD_float.h
@@ -1,6 +1,3 @@
-
-#include "SIMD_float_256.h"
-
 #if defined(__AVX512F__)
 #include "SIMD_float_512.h" // Header for AVX-512 intrinsics
 #elif defined(__AVX2__)
@@ -12,7 +9,7 @@
 #elif defined(_M_IX86_FP) && _M_IX86_FP == 1
 #include "SIMD_float_128.h" // Header for SSE intrinsics
 #elif defined(_M_X64)
-//#include "SIMD_float_128.h" // SSE2 support is implied for x64
+#include "SIMD_float_128.h" // SSE2 support is implied for x64
 #else
 #error "No SIMD support detected. Ensure your compiler supports at least SSE."
 #endif
